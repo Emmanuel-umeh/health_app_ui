13cf1525e239295ba9f9fbab96d411d9df7e9309
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-17 00:58:35 +0100

    --- Auto-Git Commit ---

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {NavigationContainer, StackActions} from "@react-navigation/native"
+import {createStackNavigator} from '@react-navigation/stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const Stack = createStackNavigator()
+export default class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        
+        <Stack.Screen name = "Home" component = {} />
+        <Stack.Screen name = "Mission" component = {} />
+      </NavigationContainer>
+    )
+    <StackActions
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
