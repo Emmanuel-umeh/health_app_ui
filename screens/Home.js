5ff637a6d05d80ad59938b7ac44bb509ed86306a
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-17 00:59:51 +0100

    --- Auto-Git Commit ---

---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {View, StyleSheet, Text} from "react-native"
+export default class Home extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    
+})
