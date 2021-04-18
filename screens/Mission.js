d028e12a07ec2b4c4ee3083b6bc6917bfad6cde9
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-18 02:30:57 +0100

    --- Auto-Git Commit ---

--- screens/Mission.js
+++ screens/Mission.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
-import { View , Text} from 'react-native'
+import { View , Text, ScrollView} from 'react-native'
+// import {  } from 'react-native-gesture-handler'
 
 export default class Mission extends Component {
     render() {
         return (
             <View>
-                <Text> Mission Screen</Text>
+            <ScrollView 
+            showsVerticalScrollIndicator = {false}
+            contentContainerStyle ={{
+                width : "100%",
+                height : "120%"
+            }}
+            >
+
+                <View style = {styles.header}>
+                    <Text style = {styles.inline}> My Mission</Text>
+
+                </View>
+                
+
+            </ScrollView>
                 
             </View>
         )
