a117542baa5c6642a44e668f7ad4cfd242237706
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-18 03:15:13 +0100

    --- Auto-Git Commit ---

--- screens/Mission.js
+++ screens/Mission.js
@@ -1,92 +1,114 @@
-import React from 'react';
-import {View,Text,StyleSheet,ScrollView,TouchableOpacity} from 'react-native';
-import * as Animatable from 'react-native-animatable';
-import Scroll from './scroll';
-// import Support from './support';
+import React, { Component } from 'react'
+import { View , Text, ScrollView, StyleSheet} from 'react-native'
+// import {  } from 'react-native-gesture-handler'
+import Scroll from "./Scroll"
 
-export default class Mission extends  React.Component{
-    render(){
-        return(
-            <View style={styles.container}>
-                <ScrollView showsVerticalScrollIndicator={false} style={{flex:1}} contentContainerStyle={{width:'100%',height:'120%'}}>
-                    <View style={styles.header}>
-                        <Text style={styles.inline}>My Mission</Text>
-                    </View>
-                    <View style={styles.memos}>
-                        <ScrollView 
-                        horizontal={true} 
-                        contentContainerStyle={{width:'200%'}}
-                        showsHorizontalScrollIndicator={false}>
-                            <Scroll bgcolor="#FFBE86" 
-                            background={require('../assets/icons/graphtwo.png')}
-                            title="Game Of Chess"
-                            month = "Sep - Nov"
-                            animation = "bounceInLeft"
-                            />
-                             <Scroll bgcolor="#7DC9E7" 
-                            background={require('../assets/icons/graphtwo.png')}
-                            title="100 Km Jogging"
-                            month = "Jan - Feb"
-                            animation = "bounceInLeft"
-                            />
-                             <Scroll bgcolor="#FFBE86" 
-                            background={require('../assets/icons/graphtwo.png')}
-                            title="Netflix and Chill"
-                            month = "March - April"
-                            />
-                             <Scroll bgcolor="#7DC9E7" 
-                            background={require('../assets/icons/graphtwo.png')}
-                            title="Video Games"
-                            month = "Aug - Sep"
-                            />
-                        </ScrollView>
-                    </View>
+export default class Mission extends Component {
+    render() {
+        return (
+            <View>
+            <ScrollView 
+
+            style = {{
+                flex : 1
+            }}
+            showsVerticalScrollIndicator = {false}
+            contentContainerStyle ={{
+                width : "100%",
+                height : "120%"
+            }}
+            >
+
+                <View style = {styles.header}>
+                    <Text style = {styles.inline}> My Mission</Text>
+
+                </View>
+
+                <View style = {styles.memos}>
+                    <ScrollView horizontal = {true}
                     
-                </ScrollView>
+                    contentContainerStyle = {{width : "200%"}}
+                    showsHorizontalScrollIndicator = {false}
+                    >
+
+                        <Scroll 
+                        bgcolor = '#FFBE86'
+                        title = "Call of Duty"
+                        month = "Sep - Nov"
+                        animation = "bounceInLeft"
+
+                        background = {require("../assets/icons/graphtwo.png")}
+                        />
+                        <Scroll 
+                        bgcolor = '#FFBE86'
+                        title = "Call of Duty"
+                        month = "Sep - Nov"
+                        animation = "bounceInLeft"
+
+                        background = {require("../assets/icons/graphtwo.png")}
+                        />
+                        <Scroll 
+                        bgcolor = '#FFBE86'
+                        title = "Free Fire"
+                        month = "Sep - Nov"
+                        animation = "bounceInLeft"
+
+                        background = {require("../assets/icons/graphtwo.png")}
+                        />
+                        <Scroll 
+                        bgcolor = '#FFBE86'
+                        title = "PUBG"
+                        month = "Sep - Nov"
+                        animation = "bounceInLeft"
+
+                        background = {require("../assets/icons/graphtwo.png")}
+                        />
+                        <Scroll 
+                        bgcolor = '#FFBE86'
+                        title = "FortNite"
+                        month = "Sep - Nov"
+                        animation = "bounceInLeft"
+
+                        background = {require("../assets/icons/graphtwo.png")}
+                        />
+
+                    </ScrollView>
+
+                </View>
+                
+
+            </ScrollView>
+                
             </View>
-        );
+        )
     }
 }
+
+
 const styles = StyleSheet.create({
     container : {
         flex : 1,
-        backgroundColor : '#fff'
+        backgroundColor : "#fff"
     },
     header : {
         flex : 1,
         left : 25,
         top : 100
+
     },
     inline : {
-        fontSize : 38,
+        fontSize : 30,
         letterSpacing : -0.5,
         fontWeight : 'bold',
         color : '#2E2E2E'
+
     },
-    memos : {
-        height : 250,
-        position : 'absolute',
-        top : 160,
-        width:'100%',
-        marginLeft : 10
-    },
-    supportview : {
-        position : 'absolute',
-        left : 25,
-        top : 420
-    },
-    support : {
-        fontSize : 25,
-        fontWeight : 'bold',
-        letterSpacing : -0.5,
-        color : '#2E2E2E'
-    },
-    rectangleone : {
-        height : 85,
-        width : 300,
-        position : 'absolute',
-        alignSelf  : 'center',
-        top : 470,
-        borderRadius : 18
-    }
+     memos : {
+         height : 250,
+         position : 'absolute',
+         top : 100,
+         width : "100%",
+         marginLeft : 10
+
+     }
 })
