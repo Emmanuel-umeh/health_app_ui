3c8019a8f7f77a198c463cd62eec004eb49da0f9
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-18 02:15:26 +0100

    --- Auto-Git Commit ---

--- screens/Card.js
+++ screens/Card.js
@@ -1,47 +1,52 @@
-import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
-import * as Animatable from "react-native-animatable"
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import * as Animatable from "react-native-animatable";
 export default class Card extends Component {
-    render() {
-        return (
-            <Animatable.View animation = {this.props.animation} duration = {1500}
-            style = {StyleSheet.cardone}
-            >
-<TouchableOpacity style = {StyleSheet.box1} >
+  render() {
+    return (
+      <Animatable.View
+        animation={this.props.animation}
+        duration={1500}
+        style={StyleSheet.cardone}
+      >
+        <TouchableOpacity style={StyleSheet.box1}>
+          <View
+            style={{
+              borderRadius: 21,
+              backgroundColor: "rgba(255,19,134,0.2)",
+              height: "100%",
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Image source={this.props.image} />
+          </View>
+        </TouchableOpacity>
 
+        <View style={styles.box2}>
+          <Text
+            style={{
+              fontSize: 20,
+              color: "#2D2D2D",
+              letterSpacing: -0.5,
+            }}
+          >
+            {this.props.title}
+          </Text>
 
-<View style ={{
-    borderRadius : 21, 
-    backgroundColor : "rgba(255,19,134,0.2)", 
-    height: "100%",
-    flex: 1,
-    alignItems : "center",
-     justifyContent : "center"
-
-}}>
-    <Image  source = {this.props.image }/>
-</View>
-
-</TouchableOpacity>
-
-<View style = {styles.box2}>
-    <Text style = {{
-        fontSize : 20, color : "#2D2D2D",
-        letterSpacing : -0.5
-    }}>{this.props.title}</Text>
-
-<Text style = {{
-        fontSize : 15, color : "#BEC4C9",
-        letterSpacing : -0.5,
-        paddingVertical : 5
-    }}>{this.props.subtitle}</Text>
-
-    
-
-</View>
-
-                
-            </Animatable.View>
-        )
-    }
+          <Text
+            style={{
+              fontSize: 15,
+              color: "#BEC4C9",
+              letterSpacing: -0.5,
+              paddingVertical: 5,
+            }}
+          >
+            {this.props.subtitle}
+          </Text>
+        </View>
+      </Animatable.View>
+    );
+  }
 }
