a9a3570a8f9aac2026c14d6e4b28905ffd46754f
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-18 02:44:12 +0100

    --- Auto-Git Commit ---

--- screens/Scroll.js
+++ screens/Scroll.js
@@ -26,6 +26,9 @@ export default class Scroll extends Component {
 
 
                 <View style = {style.insidetwo}>
+                    <ImageBackground source = {this.props.background} style = {{
+                        width : "100%"
+                    }} />
 
                 </View>
                 
