8d72b140fb9005cee734733b1d0a77af5e9b3374
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-18 01:14:54 +0100

    --- Auto-Git Commit ---

---
+++ screens/Mission.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View , Text} from 'react-native'
+
+export default class Mission extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Mission Screen</Text>
+                
+            </View>
+        )
+    }
+}
